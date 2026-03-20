425c4f05fefc98981fac78d9b6c365c921b6ac35
Alexrubiks <alexrubiks@gmail.com>
2026-03-20 14:57:19 +0100

    better models

--- core/models.py
+++ core/models.py
@@ -1,38 +1,51 @@
 from django.db import models
+from django.core.validators import MinValueValidator
 
 
 class User(models.Model):
-    wca_id = models.CharField(max_length=10)
-    pseudo = models.CharField(max_length=25)
+    wca_id = models.CharField(max_length=10, unique=True, db_index=True)
+    name = models.CharField(max_length=25, db_index=True)
+
+    def __str__(self):
+        return self.name
 
 
 class Vehicle(models.Model):
-    name = models.CharField(max_length=25)
+    name = models.CharField(max_length=25, db_index=True)
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
-    seats = models.IntegerField()
+    seats = models.IntegerField(validators=MinValueValidator(1))
+
+    def __str__(self):
+        return self.name
 
 
 class Competition(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, unique=True, db_index=True)
 
     first_day = models.DateField()
     last_day = models.DateField()
 
     location_name = models.CharField(max_length=100)
-    latitude = models.FloatField()
-    longitude = models.FloatField()
+    latitude = models.FloatField(null=False)
+    longitude = models.FloatField(null=False)
+
+    def __str__(self):
+        return self.name
 
 
 class Travel(models.Model):
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=50, unique=True, db_index=True)
 
     start_location_name = models.CharField(max_length=100)
-    start_latitude = models.FloatField()
-    start_longitude = models.FloatField()
+    start_latitude = models.FloatField(null=False)
+    start_longitude = models.FloatField(null=False)
 
     end_location_name = models.CharField(max_length=100)
-    end_latitude = models.FloatField()
-    end_longitude = models.FloatField()
+    end_latitude = models.FloatField(null=False)
+    end_longitude = models.FloatField(null=False)
 
     competition = models.ForeignKey(Competition, on_delete=models.CASCADE)
     vehicle = models.ForeignKey(Vehicle, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
--- core/serializers.py
+++ core/serializers.py
@@ -4,7 +4,7 @@ from rest_framework import serializers
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ["wca_id", "pseudo"]
+        fields = ["wca_id", "name"]
 
 class VehicleSerializer(serializers.ModelSerializer):
     class Meta:
