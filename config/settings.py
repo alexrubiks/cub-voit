af43bacadfce18888c17bc2ee9249c89ec6f5b5f
Alexrubiks <alexrubiks@gmail.com>
2026-06-08 01:21:03 +0200

    changement pour l'url prod

--- config/settings.py
+++ config/settings.py
@@ -140,8 +140,7 @@ SIMPLE_JWT = {
 CORS_ALLOW_CREDENTIALS = True
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173",
-    "http://127.0.0.1:5173",
+    "https://cubvoit.alexrubiks.fr",
 ]
 
 # PROD frontend (Vercel)
