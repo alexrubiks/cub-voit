ca6c2ba88dda6c2b9da02c4687082c82afd86f81
Alexrubiks <alexrubiks@gmail.com>
2026-06-08 02:31:58 +0200

    aaaaaaaaaa

--- config/settings.py
+++ config/settings.py
@@ -141,11 +141,11 @@ SIMPLE_JWT = {
 # CORS (FRONTEND)
 # -----------------------
 CORS_ALLOW_CREDENTIALS = True
-
-CORS_ALLOWED_ORIGINS = [
-    "https://cubvoit.alexrubiks.fr",
-    "https://cub-voit.vercel.app",
-]
+ORS_ALLOW_ALL_ORIGINS=True
+# CORS_ALLOWED_ORIGINS = [
+#     "https://cubvoit.alexrubiks.fr",
+#     "https://cub-voit.vercel.app",
+# ]
 
 CORS_ALLOW_HEADERS = list(default_headers) + [
     "authorization",
